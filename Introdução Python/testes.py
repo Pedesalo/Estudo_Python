553194efc63ccc28a525d1c1d6920ac976264628
Pedesalo <claus_katzer@estudante.sc.senai.br>
2026-05-12 10:01:39 -0300

    Questão 16

--- Introdução Python/testes.py
+++ Introdução Python/testes.py
@@ -1,9 +1,8 @@
-# 15. Dada a lista [10, 20, 30, 40, 50], use um loop para calcular a média dos valores.
+# 16. Crie uma lista de 5 números, ordene-os e exiba o menor e o maior valor.
 
-lista = [10, 20, 30, 40, 50]
-print(sum(lista)/len(lista))
-print(id(lista))
-print(type(lista))
-print(dir(lista))
-print(max(lista))
+lista = [1,3,2,5,4]
+listaOrdem = sorted(lista)
+listaMenor = min(lista)
+listaMaior = max(lista)
 
+print(f"Os números são: {lista}, em ordem crescente fica: {listaOrdem}, o maior número é: {listaMaior}, e o menor número é: {listaMenor}.")
