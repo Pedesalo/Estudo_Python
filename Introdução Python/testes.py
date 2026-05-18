1a90de0a2233e50f33f583d2fba5d3e948576d3f
Pedesalo <claus_katzer@estudante.sc.senai.br>
2026-05-18 08:57:54 -0300

    Dificuldades, preciso de mais pesquisa em indices e dicionarios

--- Introdução Python/testes.py
+++ Introdução Python/testes.py
@@ -1,25 +1,16 @@
-import customtkinter as ctk
+# Lista de temperaturas (da segunda-feira passada até domingo)
+temperaturas = [28.5, 30.1, 29.0, 32.4, 26.8, 25.0, 27.3]
+dias = {"Segunda":0,"Terça":1,"Quarta":2,"Quinta":3,"Sexta":4,"Sábado":5,"Domingo":6}
 
-# Configurações de aparência
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("green")
+# --- SEU OBJETIVO ---
+# 1. Acesse e imprima a temperatura de quarta-feira (quarto elemento).
+print(f"Temperatura de Quarta-Feira é {temperaturas[dias['Quarta']]}")
+# 2. Acesse e imprima a temperatura do último dia (domingo) usando índice negativo.
+print(f'Temperaduta de Domingo é {temperaturas[-6]}')
+# 3. Extraia uma sublista apenas com os dias do fim de semana (sexta, sábado e domingo).
+print(f'As Temperaturas de Sexta-feira até Domingo serão: {temperaturas[4:7]}')
 
-def acao_botao():
-    produto = {"nome": "teclado gamer", "preço": 250.00}
-    nome_formatado = produto["nome"].capitalize()
-    label.configure(text=f"{nome_formatado} selecionado!")
-
-# Criando a janela principal
-janela = ctk.CTk()
-janela.geometry("400x240")
-janela.title("Minha Primeira Interface")
-
-# Adicionando elementos (Widgets)
-label = ctk.CTkLabel(janela, text="Bem-vindo ao Python!", font=("Roboto", 20))
-label.pack(pady=20)
-
-botao = ctk.CTkButton(janela, text="Clique Aqui", command=acao_botao)
-botao.pack(pady=10)
-
-# Iniciando o programa
-janela.mainloop()
+# --- COMENTÁRIO GUIA (Siga estes passos):
+# Passo 1: Lembre-se que o Python começa a contar os índices do 0. Então, segunda é 0, terça é 1...
+# Passo 2: Para pegar o último elemento direto, use o índice [-1].
+# Passo 3: No fatiamento [inicio:fim], o índice de 'fim' não é incluído. Descubra onde começa a sexta-feira e vá até o final.
