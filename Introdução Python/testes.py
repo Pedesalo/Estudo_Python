f5955e1f460f0eb413af71b9e8bdccfbe9e713ed
Pedesalo <clausinhokatzer@gmail.com>
2026-06-01 09:36:58 -0300

    Desavio 6 conluido

--- Introdução Python/testes.py
+++ Introdução Python/testes.py
@@ -1,17 +1,11 @@
-# 1. Crie uma lista inicial com 10 números misturados (pares e ímpares).
-pares_impares = [1,2,3,4,5,6,7,8,9,10]
-# 2. Crie duas listas novas e vazias: 'pares' e 'impares'.
-pares = []
-impares = []
-# 3. Use um laço 'for' para avaliar cada número da lista inicial.
-for i in range(len(pares_impares)):
-# 4. Se o número for divisível por 2 (num % 2 == 0), adicione na lista 'pares'.
-    if pares_impares[i] % 2 == 0:
-        pares.append(pares_impares[i])
-# 5. Se não for, adicione na lista 'impares'.
-    else:
-         impares.append(pares_impares[i])
-# 6. Imprima as três listas ao final.
-print(f'lista: {pares_impares}')
-print(f'pares: {pares}')
-print(f'impares: {impares}')
+# --- Desafio 6 (Estatísticas) ---
+# 1. Crie uma lista com 5 notas de alunos (números com ponto flutuante).
+nota = [4.0,5.5,9.5,8.5,10.0]
+# 2. Calcule a média: some todos os elementos (sum) e divida pelo total (len).
+media = sum(nota)/len(nota)
+# 3. Descubra a maior nota da lista usando a função max().
+maior = max(nota)
+# 4. Descubra a menor nota da lista usando a função min().
+menor = min(nota)
+# 5. Imprima a média, a maior e a menor nota na tela.
+print(f'A média das notas é: {media}. A maior nota é: {maior}. A menor nota é: {menor}')
