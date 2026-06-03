45b2a4e4c115eef3e80be0ea391d0c9b953d67be
Pedesalo <clausinhokatzer@gmail.com>
2026-06-03 10:26:02 -0300

    Desafio 8 melgorado

--- Introdução Python/desafio_listas.py
+++ Introdução Python/desafio_listas.py
@@ -111,10 +111,19 @@ print(f" A lista invertida é : {invertida}")
 
 # --- Desafio 8 (Ordenação Simples - Bubble Sort) ---
 # 1. Crie uma lista com 8 números totalmente desordenados.
+numeros = [9,6,1,3,6,2,4,25]
 # 2. Crie dois laços 'for' aninhados (um dentro do outro) para comparar os vizinhos.
 # 3. O laço interno deve comparar se o elemento atual é maior que o próximo elemento.
 # 4. Se for maior, você deve trocar os dois de posição na lista.
 # 5. Repita o processo até que nenhum elemento precise ser trocado. Imprima a lista final.
+for j in range(8):
+    for i in range(7):
+        if numeros[i] < numeros[int(i+1)]:
+            n1 = numeros[i]
+            n2 = numeros[i+1]
+            numeros[i] = n2
+            numeros[i+1] = n1
+print(numeros)
 
 
 # --- Desafio 9 (Matrizes / Lista de Listas) ---
--- Introdução Python/testes.py
+++ Introdução Python/testes.py
@@ -1,12 +1,13 @@
 # --- Desafio 8 (Ordenação Simples - Bubble Sort) ---
 # 1. Crie uma lista com 8 números totalmente desordenados.
 numeros = [9,6,1,3,6,2,4,25]
+voltas = len(numeros)
 # 2. Crie dois laços 'for' aninhados (um dentro do outro) para comparar os vizinhos.
 # 3. O laço interno deve comparar se o elemento atual é maior que o próximo elemento.
 # 4. Se for maior, você deve trocar os dois de posição na lista.
 # 5. Repita o processo até que nenhum elemento precise ser trocado. Imprima a lista final.
-for j in range(8):
-    for i in range(7):
+for j in range(voltas):
+    for i in range(voltas-1):
         if numeros[i] < numeros[int(i+1)]:
             n1 = numeros[i]
             n2 = numeros[i+1]
