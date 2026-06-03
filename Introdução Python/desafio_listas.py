37236e7e0e806daa0e47524a7d07354ec913e139
Pedesalo <clausinhokatzer@gmail.com>
2026-06-03 10:22:22 -0300

    Desafio 8, as vezes jogar TheFarmerWasReplaced abre sua mente

--- Introdução Python/desafio_listas.py
+++ Introdução Python/desafio_listas.py
@@ -101,11 +101,12 @@ palavras = ['anatel','marmelada','mamaqui','mamaco','papaquica']
 # 2. Crie uma lista vazia chamada 'invertida'.
 invertida = []
 # 3. Use um laço 'for' que comece no último índice e vá até o primeiro (andando para trás).
-for i in range(len(palavras),0):
+for i in range(len(palavras),0,-1):
 # 4. Dica: você pode usar range(len(lista) - 1, -1, -1) para gerar os índices invertidos.
 # 5. Adicione os elementos na lista 'invertida' com base nesses índices e imprima o resultado.
-    invertida.append(palavras[i])
-print(f'{invertida}')
+    local = i-1
+    invertida.append(palavras[local])
+print(f" A lista invertida é : {invertida}")
 
 
 # --- Desafio 8 (Ordenação Simples - Bubble Sort) ---
--- Introdução Python/testes.py
+++ Introdução Python/testes.py
@@ -1,12 +1,15 @@
-# --- Desafio 7 (Inversão Sem Função Nativa) ---
-# 1. Crie uma lista com 5 palavras quaisquer.
-palavras = ['anatel','marmelada','mamaqui','mamaco','papaquica']
-# 2. Crie uma lista vazia chamada 'invertida'.
-invertida = []
-# 3. Use um laço 'for' que comece no último índice e vá até o primeiro (andando para trás).
-for i in range(len(palavras),0,-1):
-# 4. Dica: você pode usar range(len(lista) - 1, -1, -1) para gerar os índices invertidos.
-# 5. Adicione os elementos na lista 'invertida' com base nesses índices e imprima o resultado.
-    local = i-1
-    invertida.append(palavras[local])
-print(f" A lista invertida é : {invertida}")
+# --- Desafio 8 (Ordenação Simples - Bubble Sort) ---
+# 1. Crie uma lista com 8 números totalmente desordenados.
+numeros = [9,6,1,3,6,2,4,25]
+# 2. Crie dois laços 'for' aninhados (um dentro do outro) para comparar os vizinhos.
+# 3. O laço interno deve comparar se o elemento atual é maior que o próximo elemento.
+# 4. Se for maior, você deve trocar os dois de posição na lista.
+# 5. Repita o processo até que nenhum elemento precise ser trocado. Imprima a lista final.
+for j in range(8):
+    for i in range(7):
+        if numeros[i] < numeros[int(i+1)]:
+            n1 = numeros[i]
+            n2 = numeros[i+1]
+            numeros[i] = n2
+            numeros[i+1] = n1
+print(numeros)
